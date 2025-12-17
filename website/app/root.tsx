52405b00698e879aa2af84cdce4e661e594a7fed
Greg Pstrucha <875316+Gricha@users.noreply.github.com>
2025-12-16 23:16:53 -0800

    Font loading

--- website/app/root.tsx
+++ website/app/root.tsx
@@ -21,10 +21,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=IM+Fell+English:ital@0;1&family=Shadows+Into+Light&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-
         <Meta />
         <Links />
       </head>
